72af68003e5107958377fa4f1b32c4dcabede0b7
Zak Nye <zaknye@gmail.com>
2023-03-01 00:53:36 +0000

    Break out xcel class to own file, fix endpoints.yaml mistake

--- main.py
+++ main.py
@@ -1,145 +1,22 @@
 import os
 import ssl
 import yaml
-from copy import deepcopy
 import requests
 import paho.mqtt.client as mqtt
 import xml.etree.ElementTree as ET
-from pathlib import Path
 from time import sleep
+from pathlib import Path
+from xcelEndpoint import xcelEndpoint
 from zeroconf import ServiceBrowser, ServiceListener, Zeroconf
 from requests.packages.urllib3.util.ssl_ import create_urllib3_context
 from requests.packages.urllib3.poolmanager import PoolManager
 from requests.adapters import HTTPAdapter
 
+# Prefix that appears on all of the XML elements
 IEEE_PREFIX = '{urn:ieee:std:2030.5:ns}'
-MQTT_TOPIC = 'xcel_itron2mqtt/meter_reading/'
 POLLING_RATE = 5
-
 # Our target cipher is: ECDHE-ECDSA-AES128-CCM8
 CIPHERS = ('ECDHE')
-
-class XcelQuery():
-    """
-    Class wrapper for all readings associated with the Xcel meter.
-    Expects a request session that should be shared amongst the 
-    instances.
-    """
-    def __init__(self, session: requests.session, url: str, name: str, 
-                    tags: list, poll_rate = 5.0):
-        self.requests_session = session
-        self.url = url
-        self.name = name
-        self.tags = tags
-        #self.client = mqtt_client
-        self.poll_rate = poll_rate
-
-        self._mqtt_topic_prefix = 'homeassistant/'
-        self._current_response = None
-        
-    def query_endpoint(self) -> str:
-        """
-        Sends a request to the given endpoint associated with the 
-        object instance
-
-        Returns: str in XML format of the meter's response
-        """
-        x = self.requests_session.get(self.url, verify=False, timeout=4.0)
-    
-        return x.text
-
-    def parse_response(self, response: str, tags: dict) -> dict:
-        """
-        Drill down the XML response from the meter and extract the
-        readings according to the endpoints.yaml structure.
-
-        Returns: dict in the nesting structure of found below each tag
-        in the endpoints.yaml
-        """
-        readings_dict = {}
-        root = ET.fromstring(response)
-        # Kinda gross
-        for k, v in tags.items():
-            if isinstance(v, list):
-                for val_items in v:
-                    for k2, v2 in val_items.items():
-                        if not readings_dict.keys():
-                            readings_dict[k] = {}
-                        search_val = f'{IEEE_PREFIX}{k2}'
-                        value = root.find(f'.//{search_val}').text
-                        readings_dict[k][k2] = value
-            else:
-                search_val = f'{IEEE_PREFIX}{k}'
-                value = root.find(f'.//{IEEE_PREFIX}{k}').text
-                readings_dict[k] = value
-    
-        return readings_dict
-
-    def get_reading(self) -> dict:
-        """
-        Query the endpoint associated with the object instance and
-        return the parsed XML response in the form of a dictionary
-        
-        Returns: Dict in the form of {reading: value}
-        """
-        response = self.query_endpoint()
-        self.current_response = self.parse_response(response, self.tags)
-
-        return self.current_response
-
-    def create_config(self, sensor_name: str, name_suffix: str, details: dict) -> tuple[str, dict]:
-        """
-        Helper to generate the JSON sonfig payload for setting
-        up the new Homeassistant entities
-
-        Returns: Tuple consisting of a string representing the mqtt
-        topic, and a dict to be used as the payload.
-        """
-        payload = deepcopy(details)
-        entity_type = payload.pop('entity_type')
-        payload['state_topic'] = f'{self._mqtt_topic_prefix}{entity_type}/{self.name}/state'
-        payload['value_template'] = f"{{{{ value_template.{sensor_name} }}}}"
-        mqtt_topic = f'{self._mqtt_topic_prefix}{entity_type}/{self.name}{name_suffix}/config'
-
-        return mqtt_topic, payload
-
-    def mqtt_send_config(self) -> None:
-        """
-        Homeassistant requires a config payload to be sent to more
-        easily setup the sensor/device once it appears over mqtt
-        https://www.home-assistant.io/integrations/mqtt/
-        """
-        _tags = deepcopy(self.tags)
-        for k, v in _tags.items():
-            if isinstance(v, list):
-                for val_items in v:
-                    name, details = val_items.popitem()
-                    name_suffix = f'{k[0].upper()}{name[0].upper()}'
-                    sensor_name = f'{k}{name}'
-                    mqtt_topic, payload = self.create_config(sensor_name, name_suffix, details)
-                    print(f"Sending to MQTT TOPIC:\t{mqtt_topic}")
-                    print(f"Payload:\t\t{payload}")
-                    # Send MQTT payload
-            else:
-                name_suffix = f'{k[0].upper()}'
-                mqtt_topic, payload = self.create_config(k, name_suffix, v)
-                print(f"Sending to MQTT TOPIC:\t{mqtt_topic}")
-                print(f"Payload:\t\t{payload}")
-    
-    def mqtt_create_message() -> str:
-
-        return
-
-    def mqtt_publish(messsage: str) -> int:
-        """
-        Publish the given message to the topic associated with the class
-       
-        Returns status integer
-        """
-        result = client.publish(topic, message)
-        
-        # Return status of the published message
-        return result[0]
 
 # Create an adapter for our request to enable the non-standard cipher
 # From https://lukasa.co.uk/2017/02/Configuring_TLS_With_Requests/
@@ -255,10 +132,9 @@ def mDNS_search_for_meter() -> str:
 
     Returns: string, ip address of the meter
     """
-    # create a zeroconf object and listener to query mDNS for the meter
     zeroconf = Zeroconf()
     listener = XcelListener()
-    # Meter will respone on _smartenergy._tcp.local. port 5353
+    # Meter will respond on _smartenergy._tcp.local. port 5353
     browser = ServiceBrowser(zeroconf, "_smartenergy._tcp.local.", listener)
     # Have to wait to hear back from the asynchrounous listener/browser task
     sleep(10)
@@ -290,7 +166,7 @@ if __name__ == '__main__':
         for endpoint_name, v in point.items():
             request_url = f'https://{ip_address}:8081{v["url"]}'
             #query_obj.append(XcelQuery(session, request_url, endpoint_name, v['tags'], mqtt_client))
-            query_obj.append(XcelQuery(session, request_url, endpoint_name, v['tags']))
+            query_obj.append(xcelEndpoint(session, request_url, endpoint_name, v['tags']))
     
     # Send MQTT config setup to Home assistant
     for obj in query_obj:
