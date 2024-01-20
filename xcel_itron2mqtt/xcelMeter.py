ad61ff6cc157ae97df41f0d3a08d58d14fe359e9
Stafford Brunk <wingrunr21@gmail.com>
2024-01-20 13:57:56 -0700

    improvement: DRY up ssl context creation in CCM8Adapter

--- xcel_itron2mqtt/xcelMeter.py
+++ xcel_itron2mqtt/xcelMeter.py
@@ -27,22 +27,20 @@ class CCM8Adapter(HTTPAdapter):
     Not really sure how much redundancy is actually required here
     """
     def init_poolmanager(self, *args, **kwargs):
-        ssl_version=ssl.PROTOCOL_TLSv1_2
-        context = create_urllib3_context(ssl_version=ssl_version)
-        context.check_hostname = False
-        context.verify_mode = ssl.CERT_REQUIRED
-        context.set_ciphers(CIPHERS)
-        kwargs['ssl_context'] = context
+        kwargs['ssl_context'] = self.create_ssl_context()
         return super(CCM8Adapter, self).init_poolmanager(*args, **kwargs)
 
     def proxy_manager_for(self, *args, **kwargs):
+        kwargs['ssl_context'] = self.create_ssl_context()
+        return super(CCM8Adapter, self).proxy_manager_for(*args, **kwargs)
+
+    def create_ssl_context(self):
         ssl_version=ssl.PROTOCOL_TLSv1_2
         context = create_urllib3_context(ssl_version=ssl_version)
         context.check_hostname = False
         context.verify_mode = ssl.CERT_REQUIRED
         context.set_ciphers(CIPHERS)
-        kwargs['ssl_context'] = context
-        return super(CCM8Adapter, self).proxy_manager_for(*args, **kwargs)
+        return context
 
 class xcelMeter():
 
