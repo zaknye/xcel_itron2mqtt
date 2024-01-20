14c3cbd8e89ec001b07bf93f44d62919db035cac
Stafford Brunk <wingrunr21@gmail.com>
2024-01-20 13:59:36 -0700

    fix: remove 4s timeout on xcelEndpoint
    The meter is flaky so allow more time to get a response

--- xcel_itron2mqtt/xcelEndpoint.py
+++ xcel_itron2mqtt/xcelEndpoint.py
@@ -39,7 +39,7 @@ class xcelEndpoint():
 
         Returns: str in XML format of the meter's response
         """
-        x = self.requests_session.get(self.url, verify=False, timeout=4.0)
+        x = self.requests_session.get(self.url, verify=False)
     
         return x.text
 
